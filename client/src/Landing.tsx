d625d7e4ce68cb38faf45b328841e502e7ef0661
Eric Zepeda <zepedaeric@gmail.com>
2020-12-08 21:28:52 -0800

    Add flex display properties to Container

--- client/src/Landing.tsx
+++ client/src/Landing.tsx
@@ -5,12 +5,17 @@ const Container = styled.div`
 height: 100vh;
 background: rgb(74,142,231);
 background: linear-gradient(180deg, rgba(74,142,231,1) 0%, rgba(169,244,173,1) 100%);
+display: flex;
+flex-direction: column;
+justify-content: center;
+
 `
 
 const Landing:React.FC = () => {
   // Should include title, and "Join Game" / "Create a Game" buttons. Possibly option to view ongoing public games
   return (
-    <Container></Container>
+    <Container>
+    </Container>
   );
 }
 
