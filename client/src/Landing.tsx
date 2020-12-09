62768fc840ee4ee4d02a559ba63d3340b70c1425
Eric Zepeda <zepedaeric@gmail.com>
2020-12-08 21:24:45 -0800

    Set Container Div equal to fullscreen

--- client/src/Landing.tsx
+++ client/src/Landing.tsx
@@ -1,9 +1,16 @@
 import * as React from 'react';
+import styled from 'styled-components'
+
+const Container = styled.div`
+height: 100vh;
+background: rgb(74,142,231);
+background: linear-gradient(180deg, rgba(74,142,231,1) 0%, rgba(169,244,173,1) 100%);
+`
 
 const Landing:React.FC = () => {
   // Should include title, and "Join Game" / "Create a Game" buttons. Possibly option to view ongoing public games
   return (
-    <div>Landing Page</div>
+    <Container></Container>
   );
 }
 
